9e9b390dc230b782badeb50e09d8d1d2a603acba
Nvmanh472001 <nguyenvanmanh_t64@hus.edu.vn>
2022-12-05 11:43:05 +0700

    add classification file

--- modules/classification/layoutlm_v3.py
+++ modules/classification/layoutlm_v3.py
@@ -1,4 +1,4 @@
-from transformers import LayoutLMv3Model, LayoutLMv3Processor
+from transformers import LayoutLMv3Processor, LayoutLMv3ForTokenClassification
 
 
 class LayoutLMv3:
@@ -7,3 +7,50 @@ class LayoutLMv3:
         self.config = config
 
         self.processor = LayoutLMv3Processor.from_pretrained(weights_path)
+        self.model = LayoutLMv3ForTokenClassification.from_pretrained(weights_path)
+
+    def _encode(self, image, words, boxes, word_labels):
+        encoding = self.processor(image, words,
+                                  boxes=boxes, word_labels=word_labels,
+                                  return_tensors='pt', truncation=True)
+
+        encode_value = {
+            'value': encoding,
+            'image_shape': image.size
+        }
+
+        return encode_value
+
+    def _unnormalize_box(self, bbox, width, height):
+        return [
+            width * (bbox[0] / 1000),
+            height * (bbox[1] / 1000),
+            width * (bbox[2] / 1000),
+            height * (bbox[3] / 1000),
+        ]
+
+    def _predict(self, encode_value):
+        import torch
+        with torch.no_grad():
+            encoding = encode_value['value']
+            width, height = encoding['image_shape']
+
+            outputs = self.model(**encoding)
+
+            logits = outputs.logits
+
+            predictions = logits.argmax(-1).squeeze().tolist()
+            labels = encoding.labels.squeeze().tolist()
+
+            token_boxes = encoding.bbox.squeeze().tolist()
+
+            true_predictions = [self.model.config.id2label[pred] for pred, label in zip(predictions, labels)
+                                if label != - 100]
+
+            true_boxes = [self._unnormalize_box(box, width, height) for box, label in zip(token_boxes, labels)
+                          if label != -100]
+
+            return ( true_predictions, true_boxes )
+
+    def _draw_image(self, image):
+        pass
